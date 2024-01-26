17ccc973083dc8506183ecf99c2b0024c44992f4
Emiley Lauritzen <11000183@uvu.edu>
2024-01-26 15:22:44 -0700

    challenge1a DAGV 1400
    I am fully ready to accept this not being enough

---
+++ DAGV_1400/ConsoleApp1/ConsoleApp1/unit1achallenge.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      //get a username
+      Console.WriteLine("Please input a user name:");
+      string username = Console.ReadLine();
+      
+      //get one number
+      Console.WriteLine("Please input the first number:");
+      int firstInput = Convert.ToInt32(Console.ReadLine());
+      
+      //get a second number
+      Console.WriteLine("Please input the second number:");
+      int secondInput = Convert.ToInt32(Console.ReadLine());
+
+      //display them added
+      Console.WriteLine("Them added: " + (firstInput+secondInput));
+
+      //display them subtracted
+      Console.WriteLine("Them subtracted: " + (firstInput-secondInput));
+
+      //thank the user
+      Console.WriteLine("Thank you " + username);
+    }
+  }
+}
+
+      
